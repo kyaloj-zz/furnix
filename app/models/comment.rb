5bebc5d4356471e52c4cec2ef2f9ec644f4f53d3
justus <jmbaluka@mfarm.co.ke>
2015-05-24 08:38:34 +0300

    finished working on all shops page

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ActiveRecord::Base
 	belongs_to :furniture
-	belongs_to :user
+	
 end
--- app/models/furniture.rb
+++ app/models/furniture.rb
@@ -3,6 +3,7 @@ class Furniture < ActiveRecord::Base
 	has_many :photos
 	belongs_to :shop
 
+
     has_attached_file :avatar
 	validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
