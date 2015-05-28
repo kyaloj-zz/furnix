f3ef52fadc6c918431963297c95b6eb7f1c1e4e2
justus <jmbaluka@mfarm.co.ke>
2015-05-28 10:25:53 +0300

    removed name requirement in devise setup

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,11 +3,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
     before_filter :update_sanitized_params, if: :devise_controller?
 
     def update_sanitized_params
-       devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:name, :email, :password,
+       devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:email, :password,
         :password_confirmation, :avatar )}
 
        devise_parameter_sanitizer.for(:account_update) {|u| u.permit(:email, :password,
         :password_confirmation,
-        :name, :avatar, :current_password )}
+         :avatar, :current_password )}
     end
 end
