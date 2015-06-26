d2e3f47e4864ceedb2419d9e25ebbc2f0d25a668
justus <jmbaluka@mfarm.co.ke>
2015-06-26 07:53:40 +0300

    begun working on the api part of the app

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,31 @@ devise_for :users, controllers: {registrations: "users/registrations"}  # devise
   
 
    root to: 'homes#index'
+
+
+   #api
+  namespace :api do
+    namespace :v1 do
+        devise_for :users, controllers: {registrations: "users/registrations"}  # devise_for :users
+  resources :homes
+  resources :photos
+
+  resources :shops do
+            resources :furnitures 
+                
+           end
+
+  resources :furnitures do
+   resources :comments
+ end
+
+ 
+  resources :users
+   
+  
+
+   root to: 'homes#index'
+    end
+  end
   
 end
