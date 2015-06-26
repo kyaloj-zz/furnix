c3327b25efdf7014778b4714675e4a4813da5fc7
justus <jmbaluka@mfarm.co.ke>
2015-06-26 09:52:00 +0300

    copied all controlers to the api/v1 folder

---
+++ app/controllers/api/v1/furnitures_controller.rb
@@ -0,0 +1,77 @@
+class FurnituresController < ApplicationController
+  before_action :set_furniture, only: [:show, :edit, :update, :destroy]
+
+ 
+  def index
+    @furnitures = Furniture.all
+    
+  end
+
+ 
+  def show
+    @furniture = Furniture.find(params[:id])
+    @comment = Comment.new
+    @comment.furniture_id = @furniture.id
+    @furnitures = Furniture.last(6)
+
+
+    
+  end
+
+  
+  def new
+    @furniture = Furniture.new
+  end
+
+ 
+  def edit
+  end
+
+ 
+  def create
+    @furniture = Furniture.new(furniture_params)
+    @furniture.shop_id = params[:shop_id]
+    
+    respond_to do |format|
+      if @furniture.save
+        format.html { redirect_to @furniture, notice: 'Furniture was successfully created.' }
+        format.json { render :show, status: :created, location: @furniture }
+      else
+        format.html { render :new }
+        format.json { render json: @furniture.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+ 
+  def update
+    respond_to do |format|
+      if @furniture.update(furniture_params)
+        format.html { redirect_to @furniture, notice: 'Furniture was successfully updated.' }
+        format.json { render :show, status: :ok, location: @furniture }
+      else
+        format.html { render :edit }
+        format.json { render json: @furniture.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+ 
+  def destroy
+    @furniture.destroy
+    respond_to do |format|
+      format.html { redirect_to furnitures_url, notice: 'Furniture was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_furniture
+      @furniture = Furniture.find(params[:id])
+    end
+
+    
+    def furniture_params
+      params.require(:furniture).permit(:shop_id, :title, :description, :avatar, :price, :stock, :category, :delivery)
+    end
+end
