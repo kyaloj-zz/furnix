0ca8a32dbd48e1cf3c3783a9530d1546645a92b2
Justus <mj.kyalo@gmail.com>
2015-12-09 23:10:18 +0300

    update homepage

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,69 +3,11 @@ class HomesController < ApplicationController
 
   def index
     @homes = Home.all
-    @furnitures = Furniture.last(6)
+    @furnitures = Furniture.last(8)
     @shops = Shop.last(4)
 
   end
-
   
   def show
   end
-
-  
-  # def new
-  #   @home = Home.new
-  # end
-
-  
-  # def edit
-  # end
-
-  
-  # def create
-  #   @home = Home.new(home_params)
-
-  #   respond_to do |format|
-  #     if @home.save
-  #       format.html { redirect_to @home, notice: 'Home was successfully created.' }
-  #       format.json { render :show, status: :created, location: @home }
-  #     else
-  #       format.html { render :new }
-  #       format.json { render json: @home.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
-  
-  # def update
-  #   respond_to do |format|
-  #     if @home.update(home_params)
-  #       format.html { redirect_to @home, notice: 'Home was successfully updated.' }
-  #       format.json { render :show, status: :ok, location: @home }
-  #     else
-  #       format.html { render :edit }
-  #       format.json { render json: @home.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
-  # on
-  # def destroy
-  #   @home.destroy
-  #   respond_to do |format|
-  #     format.html { redirect_to homes_url, notice: 'Home was successfully destroyed.' }
-  #     format.json { head :no_content }
-  #   end
-  # end
-
-  # private
-  
-  #   def set_home
-  #     @home = Home.find(params[:id])
-  #   end
-
-    
-  #   def home_params
-  #     params[:home]
-  #   end
 end
