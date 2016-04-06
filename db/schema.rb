5862b2817fa372a122663ebe742976f1997796f2
Justus <mj.kyalo@gmail.com>
2016-04-06 11:37:14 +0300

    update database.yml

--- db/schema.rb
+++ db/schema.rb
@@ -14,27 +14,27 @@
 ActiveRecord::Schema.define(version: 20151215151459) do
 
   create_table "comments", force: :cascade do |t|
-    t.text     "body"
-    t.integer  "user_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-    t.integer  "furniture_id"
+    t.text     "body",         limit: 65535
+    t.integer  "user_id",      limit: 4
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.integer  "furniture_id", limit: 4
   end
 
   create_table "furnitures", force: :cascade do |t|
-    t.integer  "shop_id"
-    t.string   "title"
-    t.text     "description"
-    t.string   "image"
-    t.decimal  "price"
-    t.integer  "stock"
-    t.string   "category"
-    t.string   "delivery"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
+    t.integer  "shop_id",             limit: 4
+    t.string   "title",               limit: 255
+    t.text     "description",         limit: 65535
+    t.string   "image",               limit: 255
+    t.decimal  "price",                             precision: 10
+    t.integer  "stock",               limit: 4
+    t.string   "category",            limit: 255
+    t.string   "delivery",            limit: 255
+    t.datetime "created_at",                                       null: false
+    t.datetime "updated_at",                                       null: false
+    t.string   "avatar_file_name",    limit: 255
+    t.string   "avatar_content_type", limit: 255
+    t.integer  "avatar_file_size",    limit: 4
     t.datetime "avatar_updated_at"
   end
 
@@ -44,57 +44,57 @@ ActiveRecord::Schema.define(version: 20151215151459) do
   end
 
   create_table "photos", force: :cascade do |t|
-    t.string   "image"
-    t.integer  "furniture_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.string   "image",        limit: 255
+    t.integer  "furniture_id", limit: 4
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
   create_table "shops", force: :cascade do |t|
-    t.string   "name"
-    t.string   "location"
-    t.string   "contact_info"
-    t.string   "age"
-    t.string   "payment_mode"
-    t.integer  "user_id"
-    t.text     "description"
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
+    t.string   "name",                          limit: 255
+    t.string   "location",                      limit: 255
+    t.string   "contact_info",                  limit: 255
+    t.string   "age",                           limit: 255
+    t.string   "payment_mode",                  limit: 255
+    t.integer  "user_id",                       limit: 4
+    t.text     "description",                   limit: 65535
+    t.datetime "created_at",                                  null: false
+    t.datetime "updated_at",                                  null: false
+    t.string   "avatar_file_name",              limit: 255
+    t.string   "avatar_content_type",           limit: 255
+    t.integer  "avatar_file_size",              limit: 4
     t.datetime "avatar_updated_at"
-    t.string   "background_image_file_name"
-    t.string   "background_image_content_type"
-    t.integer  "background_image_file_size"
+    t.string   "background_image_file_name",    limit: 255
+    t.string   "background_image_content_type", limit: 255
+    t.integer  "background_image_file_size",    limit: 4
     t.datetime "background_image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email_address"
-    t.string   "password_digest"
-    t.string   "background_image"
-    t.string   "gender"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string   "email_address",          limit: 255
+    t.string   "password_digest",        limit: 255
+    t.string   "background_image",       limit: 255
+    t.string   "gender",                 limit: 255
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.string   "avatar_file_name",       limit: 255
+    t.string   "avatar_content_type",    limit: 255
+    t.integer  "avatar_file_size",       limit: 4
     t.datetime "avatar_updated_at"
-    t.string   "name"
+    t.string   "name",                   limit: 255
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
