90a5996e4fdd12a27cea62a815eb80bb044be618
Justus <mj.kyalo@gmail.com>
2016-04-10 09:08:11 +0300

    update uncorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,20 +1,46 @@
 # set path to application
-app_dir = File.expand_path("../..", __FILE__)
-shared_dir = "#{app_dir}/shared"
-working_directory app_dir
+app_path = File.expand_path(File.dirname(__FILE__) + '/..')
+worker_processes (ENV['RAILS_ENV'] == 'production' ? 4 : 1)
 
+# You can listen on a port or a socket. Listening on a socket is good in a
+# production environment, but listening on a port can be useful for local
+# debugging purposes.
+listen app_path + '/tmp/unicorn.sock', backlog: 64
 
-# Set unicorn options
-worker_processes 2
+# For development, you may want to listen on port 3000 so that you can make sure
+# your unicorn.rb file is soundly configured.
+listen(3000, backlog: 64) if ENV['RAILS_ENV'] == 'development'
+
+# After the timeout is exhausted, the unicorn worker will be killed and a new
+# one brought up in its place. Adjust this to your application's needs. The
+# default timeout is 60. Anything under 3 seconds won't work properly.
+timeout 300
+
+working_directory app_path
+
+# Set the location of the unicorn pid file. This should match what we put in the
+# unicorn init script later.
+pid app_path + '/tmp/unicorn.pid'
+
+# You should define your stderr and stdout here. If you don't, stderr defaults
+# to /dev/null and you'll lose any error logging when in daemon mode.
+stderr_path app_path + '/log/unicorn.log'
+stdout_path app_path + '/log/unicorn.log'
+
 preload_app true
-timeout 30
 
-# Set up socket location
-listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
+# Garbage collection settings.
+GC.respond_to?(:copy_on_write_friendly=) &&
+  GC.copy_on_write_friendly = true
 
-# Logging
-stderr_path "#{shared_dir}/log/unicorn.stderr.log"
-stdout_path "#{shared_dir}/log/unicorn.stdout.log"
+# If using ActiveRecord, disconnect (from the database) before forking.
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) &&
+    ActiveRecord::Base.connection.disconnect!
+end
 
-# Set master PID location
-pid "#{shared_dir}/pids/unicorn.pid"
+# After forking, restore your ActiveRecord connection.
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) &&
+    ActiveRecord::Base.establish_connection
+end
