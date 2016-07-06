d41ea4e56ad7fad9e9d321d18001e40a7cea1c6b
Justus <mj.kyalo@gmail.com>
2016-07-07 00:17:43 +0300

    passing all basic validations tests

--- spec/models/furniture_spec.rb
+++ spec/models/furniture_spec.rb
@@ -5,8 +5,16 @@ describe Furniture do
   it "has a valid factory" do
   	FactoryGirl.create(:furniture).should be_valid
   end
-  it "is invalid without a title"
-  it "is invalid without a description"
-  it "is invalid without a price"
-  it "is invalid without a category"
+  it "is invalid without a title" do
+  	FactoryGirl.build(:furniture, title: nil).should_not be_valid
+  end
+  it "is invalid without a description" do 
+  	FactoryGirl.build(:furniture, description: nil).should_not be_valid
+  end
+  it "is invalid without a price" do 
+  	FactoryGirl.build(:furniture, price: nil).should_not be_valid
+  end
+  it "is invalid without a category" do
+  	FactoryGirl.build(:furniture, category: nil).should_not be_valid
+  end
 end
