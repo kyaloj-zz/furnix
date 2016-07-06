6a6a5a58e8799ecc68ff0e24ed46e729c4b6627f
Justus <mj.kyalo@gmail.com>
2016-07-06 08:44:14 +0300

    add model specs description

---
+++ spec/models/furniture_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+require 'rails_helper'
+
+describe Furniture do
+  it "has a valid factory"
+  it "is invalid without a title"
+  it "is invalid without a description"
+  it "is invalid without a price"
+  it "is invalid without a category"
+end
