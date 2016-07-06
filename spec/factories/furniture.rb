a6ed9205345d3198bb58ae5be0b527a35fb23bca
Justus <mj.kyalo@gmail.com>
2016-07-06 09:20:55 +0300

    valid factory test passed

--- spec/factories/furniture.rb
+++ spec/factories/furniture.rb
@@ -1,9 +1,12 @@
 require 'faker'
+require 'rails_helper'
 FactoryGirl.define do
-  factory :contact do |f|
+  factory :furniture do |f|
     f.title { Faker::Name.title }
     f.description { Faker::Lorem.paragraph }
     f.price { Faker::Number.decimal(2) }
     f.category { Faker::Lorem.word }
+    f.avatar { Faker::Avatar.image }
+    f.delivery { Faker::Lorem.word }
   end
 end
--- spec/models/furniture_spec.rb
+++ spec/models/furniture_spec.rb
@@ -2,7 +2,9 @@ require 'spec_helper'
 require 'rails_helper'
 
 describe Furniture do
-  it "has a valid factory"
+  it "has a valid factory" do
+  	FactoryGirl.create(:furniture).should be_valid
+  end
   it "is invalid without a title"
   it "is invalid without a description"
   it "is invalid without a price"
